aa48178e1ca6496941ece3da689a8285ced3f288
ELFBratsk <vitaliy_bratsk@mail.ru>
2022-03-30 23:19:14 +0300

    Добавил Задание 3.6.3 / Added Task 3.6.3

--- 3  Тестовые фреймворки/test_3_6_3.py
+++ 3  Тестовые фреймворки/test_3_6_3.py
@@ -1,9 +1,7 @@
-import math
-import time
-from pip import __main__
 import pytest
-from selenium.webdriver.common.by import By
 from selenium import webdriver
+import time
+import math
 
 @pytest.fixture(scope="function")
 def browser():
@@ -15,26 +13,29 @@ def browser():
 
 
 @pytest.mark.parametrize('list', ["236895", "236896", '236897' , '236898' , '236899' , '236903','236904' ,'236905']  )
+#@pytest.mark.parametrize('list', ["236895"]  ) Подбирал селекторы на 1 адресе
 
 class Test:
 
     def test_url(self, browser, list):
-        answer = str(math.log(int(time.time())))
-
+        
         link = f"https://stepik.org/lesson/{list}/step/1"
+        browser.implicitly_wait(10.05)
+        # browser.implicitly_wait(10) # почему то метод .implicitly_wait(10) не сработал.
         browser.get(link)
-        time.sleep(5)
-        browser.implicity_wait(10)
-
-        browser.find_element(By.CLASS_NAME, 'ember-text-area.ember-view.textarea.string-quiz__textarea').send_keys(answer)
-
-        browser.find_element(By.CLASS_NAME, 'submit-submission').click()
-
-        welcome_text = browser.find_element(By.CLASS_NAME, "smart-hints__hint")
-        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        time.sleep(8)
+        answer = str(math.log(int(time.time())))
+        browser.find_element_by_css_selector(".ember-text-area").send_keys(answer)
+        browser.find_element_by_css_selector(".submit-submission").click()
+        time.sleep(8) 
+        welcome_text = browser.find_element_by_css_selector(".smart-hints__hint")
         assert "Correct!" in welcome_text.text
-        time.sleep(5)
+        #browser.find_element(By.CLASS_NAME, 'ember-text-area.ember-view.textarea.string-quiz__textarea').send_keys(answer)
 
+        #browser.find_element(By.CLASS_NAME, 'submit-submission').click()
 
-if __main__ == __name__:
-    pytest.main()
+        #welcome_text = browser.find_element(By.CLASS_NAME, "smart-hints__hint")
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        #assert "Correct!" in welcome_text.text
+        #time.sleep(5)
+        # ОТВЕТ "The owls are not what they seem! OvO"
