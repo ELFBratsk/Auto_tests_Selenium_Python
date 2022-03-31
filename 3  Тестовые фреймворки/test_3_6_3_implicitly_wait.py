4b5efceee54213e3eac7a28c4fb0e476b618f3dc
ELFBratsk <vitaliy_bratsk@mail.ru>
2022-03-31 08:39:29 +0300

    Добавил описание test_3_6_3_implicitly_wait / Added a description of test_3_6_3_implicitly_wait

--- 3  Тестовые фреймворки/test_3_6_3_implicitly_wait.py
+++ 3  Тестовые фреймворки/test_3_6_3_implicitly_wait.py
@@ -1,3 +1,5 @@
+# Задание: параметризация тестов
+'''Инопланетяне оставляют загадочные сообщения на Stepik в фидбеке задач на правильное решение. Мы смогли локализовать несколько url-адресов задач, где появляются кусочки сообщений. Ваша задача — реализовать автотест со следующим сценарием действий: '''
 import pytest
 from selenium import webdriver
 import time
@@ -12,22 +14,21 @@ def browser():
     browser.quit()
 
 
-@pytest.mark.parametrize('list', ["236895", "236896", '236897' , '236898' , '236899' , '236903','236904' ,'236905']  )
-#@pytest.mark.parametrize('list', ["236895"]  ) #Подбирал селекторы на 1 адресе
+@pytest.mark.parametrize('list', ["236895", "236896", '236897' , '236898' , '236899' , '236903','236904' ,'236905']  ) #Используйте маркировку pytest для параметризации и передайте в тест список ссылок в качестве параметров: 
 
 class Test:
 
     def test_url(self, browser, list):
         
-        link = f"https://stepik.org/lesson/{list}/step/1"
-        browser.implicitly_wait(10)
-        # browser.implicitly_wait(10) # почему то метод .implicitly_wait(10) не сработал.
+        link = f"https://stepik.org/lesson/{list}/step/1" # открыть страницу
+        browser.implicitly_wait(10) # дождаться пока информация на сайте прогрузится. ДЛЯ ВСЕХ ЭТАПОВ
         browser.get(link)
-        #time.sleep(8)
-        answer = str(math.log(int(time.time())))
-        browser.find_element_by_css_selector(".ember-text-area").send_keys(answer)
-        browser.find_element_by_css_selector(".submit-submission").click()
-        #time.sleep(8) 
+        answer = str(math.log(int(time.time()))) # подсчет числа для ввода в поле ответа
+        browser.find_element_by_css_selector(".ember-text-area").send_keys(answer) # ввести правильный ответ 
+        browser.find_element_by_css_selector(".submit-submission").click() # нажать кнопку "Отправить" 
+        
+        # проверить, что текст в опциональном фидбеке полностью совпадает с "Correct!"
         welcome_text = browser.find_element_by_css_selector(".smart-hints__hint")
         assert "Correct!" in welcome_text.text
     
+# ОТВЕТ: 'The owls are not what they seem! OvO'
