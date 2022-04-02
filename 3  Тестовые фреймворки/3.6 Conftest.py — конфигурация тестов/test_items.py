4f076495ba86113360c0bb1011fd245e788a5469
ELFBratsk <vitaliy_bratsk@mail.ru>
2022-04-02 21:46:33 +0300

    Добавил урок 4.1.1 и скорректировал 3.6.9 test_items.py / Added lesson 4.1.1 and adjusted 3.6.9 test_items.py

--- 3  Тестовые фреймворки/3.6 Conftest.py — конфигурация тестов/test_items.py
+++ 3  Тестовые фреймворки/3.6 Conftest.py — конфигурация тестов/test_items.py
@@ -8,5 +8,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_language_page_view(browser):
     browser.get(link)
     time.sleep(10)
-    add_to_basket = browser.find_element_by_css_selector('#add_to_basket_form button.btn-add-to-basket') # Селектор кнопки является уникальным для проверяемой страницы
-    assert add_to_basket, 'Кнопка добавления в корзину = есть.' # Есть assert
+    add_to_basket = browser.find_element_by_css_selector('#add_to_basket_form button.btn-add-to-basket') # Селектор кнопки является уникальным для проверяемой страницы ИЛИ так: browser.find_elements_by_css_selector("button.btn.btn-add-to-basket")
+    assert add_to_basket, 'Button AddToBasket not found!' # при пустом массиве тест падает
