95eafd9b022be81c97c5f31612a3935816e453f9
ELFBratsk <vitaliy_bratsk@mail.ru>
2022-04-02 19:40:11 +0300

    Добавил рецензирование 3.6.9 и уроки 3.7 / Added review 3.6.9 and lessons 3.7

---
+++ 3  Тестовые фреймворки/Для рецензирования/selenium_task_3.6.9-master/test_items.py
@@ -0,0 +1,16 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+import time
+
+def test_check_button_exists_for_different_interface_languages(browser):
+    browser.get(link)
+    # to check the correctness of work
+    time.sleep(10)
+    button = None
+	
+    try:
+        button = browser.find_element_by_css_selector(".btn-add-to-basket")
+    except ZeroDivisionError:
+        # don't need to be processed here, 'assert' is called below
+        pass
+
+    assert button != None, "The button for adding an item to the shopping cart doesn't exist"
